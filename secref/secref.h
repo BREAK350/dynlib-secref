2ba248254f9ee2ba499c63353b627579c7d74813
BREAK350 <break350@gmail.com>
2015-02-28 16:43:34 +0200

    add remove and copy meethods

--- secref/secref.h
+++ secref/secref.h
@@ -13,11 +13,61 @@ namespace dynlib {
 			Reference *prev;
 			Reference *next;
 
-		public:
-			Reference();
-			~Reference();
+			void remove() {
+				if (prev) {
+					if (next) {
+						prev->next = next;
+						next->prev = prev;
+					}
+					else {
+						prev->next = 0;
+					}
+				}
+				else {
+					if (next) {
+						next->prev = 0;
+					}
+					else {
+						delete data;
+					}
+				}
+			}
 
-			Object get()const;
+			void copy(Reference &ref) {
+				data = ref.data;
+				next = ref.next;
+				prev = &ref;
+				if (ref.next) {
+					ref.next->prev = this;
+				}
+				ref.next = this;
+			}
+
+		public:
+			Reference() {
+				data = new Object();
+				prev = 0;
+				next = 0;
+			}
+
+			Reference(Reference &ref) {
+				copy(ref);
+			}
+
+			~Reference() {
+				remove();
+			}
+
+			Object& get()const {
+				return *data;
+			}
+
+			Reference& operator = (Reference & ref) {
+				remove();
+				copy(ref);
+				return *this;
+			}
+
 		};
 	}
 }
