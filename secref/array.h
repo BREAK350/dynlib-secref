a5ce5e1af0f3140249ed8e1f78efc947b0533031
BREAK350 <break350@gmail.com>
2015-03-01 12:31:42 +0200

    add body in Array

--- secref/array.h
+++ secref/array.h
@@ -15,8 +15,45 @@ namespace dynlib {
 		template<class Object>
 		class Array {
 		private:
-			int size;
+			int arr_size;
 			Reference<Object> *arr;
+
+		public:
+			Array() {
+				arr_size = 0;
+				arr = 0;
+			}
+
+			Array(int size) {
+				if (arr_size > 0) {
+					arr_size = size;
+					arr = new Reference<Object>[arr_size];
+				}
+				else {
+					arr_size = 0;
+					arr = 0;
+				}
+			}
+
+			~Array() {
+				if (arr_size > 0) {
+					delete[]arr;
+					arr_size = 0;
+					arr = 0;
+				}
+			}
+
+			Reference<Object>&operator[](int index)const {
+				return arr[index];
+			}
+
+			int size()const {
+				return arr_size;
+			}
+
+			bool isNull()const {
+				return arr == 0;
+			}
 		};
 	}
 }
